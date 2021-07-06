dbb76e558955916ba45f14799c12cfc8959f3612
Valter <valteralvees@outlook.com>
2021-07-06 11:15:06 -0300

    Estilização inicial e Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
 const repositoryName = 'unform2';
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+
+//api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  },[]);
+
   return(
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
